e35e7a8eed71e55715e4ceb03f6704e5d2e708cd
SilverFlin <toledorusso@outlook.com>
2023-03-19 15:59:48 -0700

    feat: entidad autor

--- 03_Implementation/EditandoMundos/src/main/java/org/itson/dominio/Autor.java
+++ 03_Implementation/EditandoMundos/src/main/java/org/itson/dominio/Autor.java
@@ -5,10 +5,14 @@
 package org.itson.dominio;
 
 import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -20,8 +24,49 @@ import javax.persistence.Table;
 public class Autor implements Serializable {
 
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nombres", nullable = false, length = 100)
+    private String nombre;
+
+    @Column(name = "apellidoPaterno", nullable = false, length = 100)
+    private String apellidoPaterno;
+
+    @Column(name = "apellidoMaterno", nullable = true, length = 100)
+    private String apellidoMaterno;
+
+    @Column(name = "edad", nullable = false)
+    private int edad;
+
+    @Column(name = "nacionalidad", nullable = false)
+    private Nacionalidad nacionalidad;
+
+    @OneToMany(mappedBy = "publicacion", cascade = CascadeType.REMOVE)
+    private List<Publicacion> publicaciones;
+
+    public Autor() {
+    }
+
+    public Autor(Long id, String nombre, String apellidoPaterno, String apellidoMaterno, int edad, Nacionalidad nacionalidad, List<Publicacion> publicaciones) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.edad = edad;
+        this.nacionalidad = nacionalidad;
+        this.publicaciones = publicaciones;
+    }
+
+    public Autor(String nombre, String apellidoPaterno, String apellidoMaterno, int edad, Nacionalidad nacionalidad, List<Publicacion> publicaciones) {
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.edad = edad;
+        this.nacionalidad = nacionalidad;
+        this.publicaciones = publicaciones;
+    }
 
     public Long getId() {
         return id;
@@ -29,6 +74,54 @@ public class Autor implements Serializable {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Nacionalidad getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(Nacionalidad nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public List<Publicacion> getPublicaciones() {
+        return publicaciones;
+    }
+
+    public void setPublicaciones(List<Publicacion> publicaciones) {
+        this.publicaciones = publicaciones;
     }
 
     @Override
@@ -53,7 +146,7 @@ public class Autor implements Serializable {
 
     @Override
     public String toString() {
-        return "org.itson.dominio.Autor[ id=" + id + " ]";
+        return "Autor{" + "id=" + id + ", nombre=" + nombre + ", apellidoPaterno=" + apellidoPaterno + ", apellidoMaterno=" + apellidoMaterno + ", edad=" + edad + ", nacionalidad=" + nacionalidad + ", publicaciones=" + publicaciones + '}';
     }
-    
+
 }
---
+++ 03_Implementation/EditandoMundos/src/main/java/org/itson/dominio/Nacionalidad.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.dominio;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Toled
+ */
+@Entity
+public class Nacionalidad implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Nacionalidad)) {
+            return false;
+        }
+        Nacionalidad other = (Nacionalidad) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "org.itson.dominio.Nacionalidad[ id=" + id + " ]";
+    }
+    
+}
