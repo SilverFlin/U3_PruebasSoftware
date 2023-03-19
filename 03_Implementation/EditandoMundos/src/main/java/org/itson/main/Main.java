5a36c020099839ba3cabc7754e3f3462735a06d5
SilverFlin <toledorusso@outlook.com>
2023-03-19 15:29:42 -0700

    chore: prueba temporal en main

--- 03_Implementation/EditandoMundos/src/main/java/org/itson/main/Main.java
+++ 03_Implementation/EditandoMundos/src/main/java/org/itson/main/Main.java
@@ -3,7 +3,13 @@
  */
 package org.itson.main;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.itson.dominio.PublicacionDigital;
 import org.itson.presentacion.IniciarSesionForm;
+import org.itson.presentacion.UnitOfWork;
 
 /**
  *
@@ -12,11 +18,21 @@ import org.itson.presentacion.IniciarSesionForm;
 public class Main {
 
     public static void main(String[] args) {
-        cargarForm();
+        probarListasUnitOfWork();
     }
 
     public static void cargarForm() {
         IniciarSesionForm clienteForm = new IniciarSesionForm();
         clienteForm.setVisible(true);
+    }
+
+    public static void probarListasUnitOfWork() {
+        UnitOfWork unitOfWork = new UnitOfWork();
+
+        List<PublicacionDigital> publicacionesDigitalesLista = unitOfWork.publicacionesDigitalesRepository().lista();
+
+        for (PublicacionDigital publicacionesDigitales : publicacionesDigitalesLista) {
+            System.out.println(publicacionesDigitales);
+        }
     }
 }
